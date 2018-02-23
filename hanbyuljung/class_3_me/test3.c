3e9a0cfc8c2fbbcf1371d692731fd697ca79fa67
hanbulkr <hanbulkr@gmail.com>
2018-02-23 14:27:18 +0900

    class3_practice

---
+++ hanbyuljung/class_3_me/test3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num1 = 11,num2=5;
+	int result = num1 + num2;
+	printf("result= %d \n", result);
+	result=num1 - num2;
+	printf("result = %d\n", result);
+	result= num1*num2;
+	printf("result = %d\n", result);
+	result=num1%num2;
+	printf("result= %d\n", result);
+	result = num1 / num2;
+	printf("result = %d\n",result);
+
+	return 0;
+
+}
+
---
+++ hanbyuljung/class_3_me/test6.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+{
+
+	int num1 = 10;
+	printf("num1 << 1= %d\n",num1 << 1);
+	printf("num1 << 3= %d\n",num1 << 3);
+	printf("num1 >> 2= %d\n",num1 >> 2);
+	printf("num1 & 3= %d\n",num1 & 3);
+	printf("num1 | 16= %d\n",num1 | 16);
+	printf("num1 ^ 5= %d\n",num1 ^ 5);
+	printf("num1 ^ 3= %d\n",num1 ^ 3);
+	printf("~num1 = %d\n",~num1);
+	return 0;
+	
+
+}
