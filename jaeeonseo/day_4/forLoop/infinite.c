187e60de7ac1c75636f7dcc11dfc65dd46a86abe
JAEEON SEO <tonyseo7@gmail.com>
2018-02-26 18:00:24 +0900

    20180226학습내용

---
+++ jaeeonseo/day_4/forLoop/infinite.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i, result = 'A';
+
+
+	for(; ;)
+	{
+		printf("%c\n", result);
+	}
+
+	return 0;
+}
---
+++ jaeeonseo/homework1/04.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+//synthesis: first - start, second - end, three - times
+int syn(int start, int end, int t1, int t2)
+{
+	int res = 0, i = start;
+
+	while(i < end + 1)
+	{
+		if(((i%4) == 1) || (i%6) == 1)
+		{
+			res += i;
+		}
+
+		i++;
+	}
+
+	return res;
+}
+
+int main(void)
+{
+
+	printf("tot series sum = %d\n", syn(1, 1000, 4, 6));
+
+	return 0;
+}
+
+
