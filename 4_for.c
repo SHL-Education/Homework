6452e38f15d1d8b9d1b510f60b441a622f843736
akrndi <36683635+akrndi@users.noreply.github.com>
2018-02-27 02:17:20 +0900

    20180226 4일차 신민철

---
+++ 4_for.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int syn(int start, int end, int t1, int t2)
+{
+	int res = 0, i = start;
+
+	for(i = start; i < end + 1;i++)
+	{
+		if(((i % 4) == 1) || ((i % 6) == 1))
+		{
+			res += i;
+		}
+	}
+
+	return res;
+}
+
+int main(void)
+{
+	printf("tot series sum = %d\n", syn(1, 1000, 4, 6));
+	return 0;
+}
+
+
+
+
---
+++ 5_for.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void print_seven_series(int num)
+{
+	int i;
+
+	for(i = 0;i < num; i++ )
+	{
+		if(i < num - 1)
+		{
+			printf("%d\t", (i + 1) * 7);
+		}
+		else
+		{
+			printf("%d\n", (i + 1) * 7);
+		}
+	}
+}
+
+int main(void)
+{
+	print_seven_series(7);
+	return 0;
+}
