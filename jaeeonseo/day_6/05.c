4907f5bbaa3008dad4c9d4360dfde0dfaf4a87fb
JAEEON SEO <tonyseo7@gmail.com>
2018-02-28 17:53:31 +0900

    6일차_수업내용

---
+++ jaeeonseo/day_6/05.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+struct pos{
+	double x_pos;
+	double y_pos;
+};
+
+int main(void)
+{
+	double num;
+	struct pos position;
+
+	num = 1.2;
+
+	position.x_pos = 3.3;
+	position.y_pos = 7.7;
+
+	printf("%lf\n", position.x_pos);
+	printf("%lf\n", position.y_pos);
+
+	printf("sizeof(position) = %lu\n", sizeof(position));
+
+	return 0;
+}
