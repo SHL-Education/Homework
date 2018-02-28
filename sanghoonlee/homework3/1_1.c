bd9aac9908a5950a00ca36e61c47088de1a657ea
silenc3502 <gcccompil3r@gmail.com>
2018-02-28 10:42:27 +0900

    update

---
+++ sanghoonlee/homework3/1_1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void print_even_arr_elem(char *str)
+{
+	int i;
+
+	for(i = 0; str[i]; i++)
+	{
+		if(!(str[i] % 2))
+		{
+			printf("res = %d : %c\n", str[i], str[i]);
+		}
+	}
+}
+int main(void)
+{
+	char str[32] = "Hello Embedded World!\n";
+	print_even_arr_elem(str);
+
+	return 0;
+}
