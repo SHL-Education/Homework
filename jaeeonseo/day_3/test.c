b21a630e435a986870af48158499be8d262de8c8
silenc3502 <gcccompil3r@gmail.com>
2018-03-18 11:34:41 +0900

    clean repo

--- jaeeonseo/day_3/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-int num = 10;
-printf("%d\n",num>>2);
-
-return 0;
-}
--- jaeeonseo/day_4/macro/macro.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-#define TEST 1000
-
-int main(void)
-{
-	printf("result = %d\n", TEST);
-	return 0;
-}
