2bfd97e1a7e1c197de0de30408bef6526d72b392
hanbulkr <hanbulkr@gmail.com>
2018-04-03 02:26:10 +0900

    up making

--- hanbyuljung/class_28_me/mygameserver.c
+++ hanbyuljung/class_28_me/mygameserver.c
@@ -2,29 +2,35 @@
 // SIGALRM 은 2초 이다.
 
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<fcntl.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include<string.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <pthread.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <setjmp.h>
 
-
-typedef struct socketaddr_in si;
-typedef struct socketaddr * sap;
 
 #define BUF_SIZE	256
-#define MAX_CLNT	256
+#define MAX_CLNT	512
 
+
+typedef struct sockaddr_in		si;
+typedef struct sockaddr *		sap;
 int clnt_cnt = 0;
 int clnt_socks[MAX_CLNT];
 int data[MAX_CLNT];
 int thread_pid[MAX_CLNT];
 int idx;
 int cnt[MAX_CLNT];
+int preNum=0;
+
+jmp_buf gogo;
 
 pthread_mutex_t mtx;
+
 
 void err_handler(char *msg)
 {
@@ -43,19 +49,18 @@ void sig_handler(int signo)
 
 	// 현재 쓰레드 PID를 가져온다.
 	for(i = 0; i < clnt_cnt; i++)
-		if(thread_pid[i] == getpid())
-			cnt[i] += 1;
-
+		if(thread_pid[i] == getpid())			
+			 pthread_kill(thread_pid[i], SIGINT);
+			//cnt[i] += 1;	// 이게 현재 숫자라고 가정한다.
 	pthread_mutex_unlock(&mtx);
-
-	alarm(3);
+	// 나중에 알람을 끄고 소켓이 꺼지게 해야한다.
+	close(clnt_socks[idx-1]);
 }
 
 void sig_handler2(int signo)
 {
 	int i;
-
-	printf("Time Over!\n");
+	printf(" 박수! \n");
 
 	pthread_mutex_lock(&mtx);
 
@@ -63,7 +68,7 @@ void sig_handler2(int signo)
 	for(i = 0; i < clnt_cnt; i++)
 		if(thread_pid[i] == getpid())
 			cnt[i] += 1;
-
+	longjmp(gogo, 1);
 	pthread_mutex_unlock(&mtx);
 
 	alarm(2);
@@ -78,13 +83,20 @@ void proc_msg(char *msg, int len, int k)
 	char smsg[64] = {0};
 
 	pthread_mutex_lock(&mtx);
-
+	// k는 현재 플레이어 넘버 +1, idx가 player라 보면됨.
 	cnt[k] += 1;
+	preNum++;	// 현재 진행중인 값.
+
+	// 누구턴인지 -> (턴인사람 , 아닌사람->끝)
+	// 데이터가 지행중인 값이랑 같은지
+
 
 	if(data[k] > cmp)
-		sprintf(smsg, " %d\n", cmp);
+		sprintf(smsg, "big than %d\n", cmp);
 	else if(data[k] < cmp)
 		sprintf(smsg, "less than %d\n", cmp);
+	else if(setjmp(gogo))
+		sprintf(smsg, "박수 \n");
 	else
 	{
 		strcpy(smsg, "You win!\n");
@@ -92,7 +104,7 @@ void proc_msg(char *msg, int len, int k)
 	}
 	// 스트링에 가져다가 붙이는것. 
 	// 사실 좋은 구조는 클라이언트한테 숫자 쏘고 클라이언트 종료되게 해야함.
-	strcat(smsg, "Input Number: \n");
+	sprintf(smsg, "[%d] player Input Number: \n",k+1);
 	write(clnt_socks[k], smsg, strlen(smsg));
 
 	pthread_mutex_unlock(&mtx);
@@ -116,15 +128,16 @@ void *clnt_handler(void *arg)
 	pthread_mutex_lock(&mtx);
 	thread_pid[idx++] = getpid();
 	i = idx - 1;
+	printf("idx = %d\n",idx);
 	printf("i = %d\n", i);
-	// 아 쓰레드마다 자기가 관리하는 클라이언트의 썪이고 크리티컬 섹션이 나오지 않게 하기 위해
+
 	// 락을 걸어준 것이다.
 	write(clnt_socks[i], pattern, strlen(pattern));
 	pthread_mutex_unlock(&mtx);
 
-	alarm(3);
+	alarm(2);
 	//break point idx 2명이 들어와야 시작한다고 표시한것.
-	while((str_len = read(clnt_sock, msg, sizeof(msg))) != 0 && idx > =1)
+	while((str_len = read(clnt_sock, msg, sizeof(msg))) != 0 && idx >= 1)
 	{
 		// 잘 들어왔으니 알람을 초기화 한다.
 		alarm(0);
@@ -150,6 +163,7 @@ int main(int argc , char **argv)
 	si serv_addr, clnt_addr;
 	socklen_t addr_size;
 	pthread_t t_id;
+	int idx = 0;
 
 	if(argc != 2)
 	{
@@ -171,7 +185,7 @@ int main(int argc , char **argv)
 	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	serv_addr.sin_port = htons(atoi(argv[1]));
 
-	if(bind(serv_sock, (sp)&serv_addr, sizeof(serv_addr)) == -1)
+	if(bind(serv_sock, (sap)&serv_addr, sizeof(serv_addr)) == -1)
 		err_handler("bind() error");
 
 	if(listen(serv_sock, 5) == -1)
@@ -181,18 +195,15 @@ int main(int argc , char **argv)
 	{
 		addr_size = sizeof(clnt_addr);
 		// 클라이언트의 접근을 승인한다. 다음 클라이언트가 들어오기 전까지 블록한다.
-		clnt_sock = accept(serv_sock, (sp)&clnt_addr, &addr_size);
+		clnt_sock = accept(serv_sock, (sap)&clnt_addr, &addr_size);
 
 		thread_pid[idx++] = getpid();
-
-		// 여기서 lock을 건다. data가 꼬이지 말라고 하는것이다.
-		// 소켓파일이 서버랑 클라이언트랑 공유메모리라서.
-		// 이 순간부터 clnt_sock은 건들수 없다.
+		// idx는 사람의 수 clnt_cnt 도 사람의 수.
 
 
 		pthread_mutex_lock(&mtx);
 
-		clnt_socks[clnt_cnt++] = clnt_sock;
+		clnt_socks[clnt_cnt++] = clnt_sock; 
 	
 		pthread_mutex_unlock(&mtx);
 
@@ -202,7 +213,7 @@ int main(int argc , char **argv)
 		pthread_detach(t_id);
 		printf("Connected Client IP: %s\n", inet_ntoa(clnt_addr.sin_addr));
 	}
-
+	close(clnt_sock);
 	close(serv_sock);
 
 	return 0;
