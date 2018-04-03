003a14f8ac6f2174399047c06eb896a7ce83c5b8
hanbulkr <hanbulkr@gmail.com>
2018-04-03 21:29:02 +0900

    up 29

--- hanbyuljung/class_29_me/chat_serv_b.c
+++ hanbyuljung/class_29_me/chat_serv_b.c
@@ -5,11 +5,14 @@
 #include<pthread.h>
 #include<arpa/inet.h>
 #include<sys/socket.h>
+#include <sys/time.h>
 #include<sys/epoll.h>
+#include<malloc.h>
 
 #define BUF_SIZE		128
 #define MAX_CLNT		256
 
+typedef struct timeval	tv;
 typedef struct sockaddr_in		si;
 typedef struct sockaddr *		sp;
 
@@ -17,12 +20,31 @@ int clnt_cnt = 0;
 int clnt_socks[MAX_CLNT];
 pthread_mutex_t mtx;
 
+// 카운트를 위한 구조체 
+typedef struct __count
+{
+	int send_client[0];
+}count;
+
+
 void err_handler(char *msg)
 {
 	fputs(msg, stderr);
 	fputc('\n', stderr);
 	exit(1);
 }
+
+count *ct = 0;
+// 시간을 구해주는 함수.
+void get_runtime(tv start, tv end)
+{
+	end.tv_usec = end.tv_usec - start.tv_usec;
+	end.tv_sec = end.tv_sec - start.tv_sec;
+	end.tv_usec += end.tv_sec * 1000000;
+
+	printf("runtime = %lf sec\n", end.tv_usec / 1000000.0);
+}
+
 void send_msg(char *msg, int len)
 {
 	int i;
@@ -39,11 +61,19 @@ void *clnt_handler(void *arg)
 	int clnt_sock = *((int *)arg);
 	int str_len = 0, i;
 	char msg[BUF_SIZE];
-
-	while((str_len = read(clnt_sock , msg, sizeof(msg))) != 0)
+	char clnt_count[BUF_SIZE];
+	
+	while((str_len = read(clnt_sock , msg, sizeof(msg))) != 0){
+		// 현재 말한 횟수 세는것은 한명만 된다.
+		ct->send_client[clnt_cnt-1] += 1;
+		sprintf(clnt_count,"[<말한 횟수(1.6):%d>]\n",ct->send_client[clnt_cnt-1]);
+		write(clnt_socks[clnt_cnt-1], clnt_count, sizeof(clnt_count));
 		send_msg(msg, str_len);
+	}
 
 	pthread_mutex_lock(&mtx);
+	
+	
 
 	for(i = 0; i<clnt_cnt; i++){
 		if(clnt_sock == clnt_socks[i])
@@ -67,6 +97,22 @@ int main(int argc, char **argv)
 	si serv_addr, clnt_addr;
 	socklen_t addr_size;
 	pthread_t t_id;
+
+	unsigned int i, cnt = 0;
+	tv start, end;
+
+	ct = (count*)malloc(1024);
+
+	gettimeofday(&start, NULL);
+
+	for(i = 0; i < 777777777; i++)
+		cnt++;
+
+	gettimeofday(&end, NULL);
+
+	// 여기서 시간을 구한다.
+	get_runtime(start, end);
+
 
 	if(argc != 2)
 	{
@@ -95,10 +141,14 @@ int main(int argc, char **argv)
 	{
 		addr_size = sizeof(clnt_addr);
 		clnt_sock = accept(serv_sock,(sp)&clnt_addr, &addr_size);
+		
 
 		pthread_mutex_lock(&mtx);
+		ct->send_client[clnt_cnt] = 0;
 		clnt_socks[clnt_cnt++] = clnt_sock;
 		pthread_mutex_unlock(&mtx);
+
+		printf("clnt_cnt (사람수) : %d\n", clnt_cnt);
 
 		pthread_create(&t_id, NULL, clnt_handler, (void *) &clnt_sock);
 		pthread_detach(t_id);
