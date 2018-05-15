7cf0a22942917e4a505c4220723d52e6b08d29d9
silenc3502 <gcccompil3r@gmail.com>
2018-05-16 08:30:26 +0900

    Update All

--- sanghoonlee/lec/math/vector_3d.c
+++ sanghoonlee/lec/math/vector_3d.c
@@ -1,22 +1,34 @@
 #include "vector_3d.h"
 #include <stdio.h>
 
-void print_vec3(vec3 R)
-{
-	printf("x = %f, y = %f, z = %f\n", R.x, R.y, R.z);
-}
-
 int main(void)
 {
 	vec3 A = {3, 2, 1};
 	vec3 B = {1, 1, 1};
-	vec3 R = {0, 0, 0, vec3_add, vec3_sub};
+	vec3 X = {1, 0, 0};
+	vec3 Y = {0, 1, 0};
+	vec3 v[3] = {{0, 4, 0}, {2, 2, 1}, {1, 1, 1}};
+	vec3 w[3] = {};
+	vec3 R = {0, 0, 0, 
+			  vec3_add, vec3_sub, vec3_scale,
+			  vec3_dot, vec3_cross, print_vec3,
+			  gramschmidt_normalization};
 
 	R.add(A, B, &R);
-	print_vec3(R);
+	R.print(R);
 
 	R.sub(A, B, &R);
-	print_vec3(R);
+	R.print(R);
+
+	R.scale(3, R, &R);
+	R.print(R);
+
+	printf("A dot B = %f\n", R.dot(A, B));
+
+	R.cross(X, Y, &R);
+	R.print(R);
+
+	R.gramschmidt(v, w, R);
 
 	return 0;
 }
--- sanghoonlee/lec/math/vector_3d.h
+++ sanghoonlee/lec/math/vector_3d.h
@@ -1,18 +1,26 @@
 #ifndef __VECTOR_3D_H__
 #define __VECTOR_3D_H__
 
-typedef struct vector3d
+#include <stdio.h>
+#include <math.h>
+
+typedef struct vector3d vec3;
+
+struct vector3d
 {
 	float x;
 	float y;
 	float z;
 
-	void (* add)(struct vector3d, struct vector3d, struct vector3d *);
-	void (* sub)(struct vector3d, struct vector3d, struct vector3d *);
-	void (* scale)(float, struct vector3d);
-	void (* dot)(struct vector3d, struct vector3d);
-	void (* cross)(struct vector3d, struct vector3d);
-} vec3;
+	void (* add)(vec3, vec3, vec3 *);
+	void (* sub)(vec3, vec3, vec3 *);
+	void (* scale)(float, vec3, vec3 *);
+	float (* dot)(vec3, vec3);
+	void (* cross)(vec3, vec3, vec3 *);
+	void (* print)(vec3);
+
+	void (* gramschmidt)(vec3 *, vec3 *, vec3);
+};
 
 void vec3_add(vec3 a, vec3 b, vec3 *r)
 {
@@ -26,6 +34,42 @@ void vec3_sub(vec3 a, vec3 b, vec3 *r)
 	r->x = a.x - b.x;
 	r->y = a.y - b.y;
 	r->z = a.z - b.z;
+}
+
+void vec3_scale(float factor, vec3 a, vec3 *r)
+{
+	r->x = a.x * factor;
+	r->y = a.y * factor;
+	r->z = a.z * factor;
+}
+
+float vec3_dot(vec3 a, vec3 b)
+{
+	return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+void vec3_cross(vec3 a, vec3 b, vec3 *r)
+{
+	r->x = a.y * b.z - a.z * b.y;
+	r->y = a.z * b.x - a.x * b.z;
+	r->z = a.x * b.y - a.y * b.x;
+}
+
+void print_vec3(vec3 r)
+{
+	printf("x = %f, y = %f, z = %f\n", r.x, r.y, r.z);
+}
+
+float magnitude(vec3 v)
+{
+	return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+void gramschmidt_normalization(vec3 *arr, vec3 *res, vec3 r)
+{
+	float mag = magnitude(arr[0]);
+	r.scale(1.0 / mag, arr[0], &res[0]);
+	r.print(res[0]);
 }
 
 #endif
